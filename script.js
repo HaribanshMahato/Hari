b1f58160cfe6477ffddf280e7a867c9e425f545c
HaribanshMahato <haribanshmahat2@gmail.com>
2024-08-14 11:27:57 +0530

    First portfolio

---
+++ script.js
@@ -0,0 +1,17 @@
+const switch1 = document.getElementById("switch1");
+const modeicon = document.getElementById("mode-icon");
+switch1.onclick = (icon) => {
+  document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
+    modeicon.src = "assets/light.png";
+  } else {
+    modeicon.src = "assets/half-moon.png";
+  }
+};
+
+var typed = new Typed(".span1", {
+  strings: ["Student", "Web Developer","React Native Dev"],
+  typeSpeed: 140,
+  backSpeed: 80,
+  loop: true,
+});
